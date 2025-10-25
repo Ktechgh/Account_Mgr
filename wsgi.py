a76fa9edafe74fe98dc285e368ce2d6809fb1ec3
Ktechgh <techk657@gmail.com>
2025-10-25 23:17:30 +0000

    Refactor all

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,13 @@
 import os
 from account_mgr import app, init_db
 
-if __name__ == "__main__":
-    if os.environ.get("WERKZEUG_RUN_MAIN") == "true" or not app.debug:
-        init_db()
-    app.run(debug=True)
-
-
 # if __name__ == "__main__":
-#     init_db()
+#     if os.environ.get("WERKZEUG_RUN_MAIN") == "true" or not app.debug:
+#         init_db()
 #     app.run(debug=True)
+
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
 
