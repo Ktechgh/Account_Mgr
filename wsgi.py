4f87dadb85a54cc8c0c0570eb27839c60112efcc
Ktechgh <techk657@gmail.com>
2025-10-25 23:54:00 +0000

    Refactor all

--- wsgi.py
+++ wsgi.py
@@ -7,20 +7,20 @@ from account_mgr import app, init_db
 #     app.run(debug=True)
 
 
-if __name__ == "__main__":
-    init_db()
-    app.run(debug=True)
-
-
 # if __name__ == "__main__":
-#     # Ensure auto-migrate and seeding run in Render too
-#     with app.app_context():
-#         print("🚀 Running database initialization (Render startup)...")
-#         init_db()
+#     init_db()
+#     app.run(debug=True)
 
-#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-# else:
-#     # For Render’s Gunicorn entry point
-#     with app.app_context():
-#         print("🚀 Running database initialization (Gunicorn startup)...")
-#         init_db()
+
+if __name__ == "__main__":
+    # Ensure auto-migrate and seeding run in Render too
+    with app.app_context():
+        print("🚀 Running database initialization (Render startup)...")
+        init_db()
+
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    # For Render’s Gunicorn entry point
+    with app.app_context():
+        print("🚀 Running database initialization (Gunicorn startup)...")
+        init_db()
