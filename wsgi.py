4a078469fd825feed6c14d195c2a34a090308e2f
Ktechgh <techk657@gmail.com>
2025-10-26 11:57:55 +0000

    Ref

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ from account_mgr import app, init_db
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True, port=4000)
+    app.run(debug=True)
 
 # if __name__ == "__main__":
 #     if os.environ.get("WERKZEUG_RUN_MAIN") == "true" or not app.debug:
